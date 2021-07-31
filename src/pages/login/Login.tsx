98da6c2bb2b5ab1bb44fed160e6167f08d3ad82f
Edwin Vargas <edwinleandrovargas@gmail.com>
2021-07-31 13:46:52 -0400

    Uopdate Login page

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React from 'react'
+import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react';
 
-const Login = props => {
-    return (
-        <div>
-            Login
-        </div>
-    );
-};
+import {AsalLogo} from "../../assets";
+
+const Login = () => (
+    <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
+        <Grid.Column style={{ maxWidth: 450 }}>
+            <Header as='h2' color='teal' textAlign='center'>
+                <Image src={AsalLogo} /> Log-in to your account
+            </Header>
+            <Form size='large'>
+                <Segment stacked>
+                    <Form.Input fluid icon='user' iconPosition='left' placeholder='E-mail address' />
+                    <Form.Input
+                        fluid
+                        icon='lock'
+                        iconPosition='left'
+                        placeholder='Password'
+                        type='password'
+                    />
+
+                    <Button color='teal' fluid size='large'>
+                        Login
+                    </Button>
+                </Segment>
+            </Form>
+            <Message>
+                New to us? <a href='/registro'>Sign Up</a>
+            </Message>
+        </Grid.Column>
+    </Grid>
+)
 
 export default Login;
