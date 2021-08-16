8098c3d5fd0b6c56820e704c44aa81ded9dfb855
Edwin Vargas <edwinleandrovargas@gmail.com>
2021-08-16 02:09:21 -0400

    Add food type

--- src/pages/dashboard/components/ConsumirTab.tsx
+++ src/pages/dashboard/components/ConsumirTab.tsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from "react";
 import {getRecipes} from "../../../services";
-import {Label, Form, List, Message, Tab, Button, Dropdown} from "semantic-ui-react";
+import {Label, Form, List, Message, Tab, Button, Dropdown, Input} from "semantic-ui-react";
 import {ColumnFood} from "../../recetas/styled";
 
 type Receta = {
@@ -8,9 +8,14 @@ type Receta = {
     id: number;
 }
 
+const FoodTypes = ['desayuno', 'almuerzo', 'cena', 'merienda'].map((foodType, i) => ({
+    text: foodType,
+    key: i,
+    value: i
+}));
+
 function useRecetas() {
     const [data, setData] = useState<null | Receta[]>(null);
-
     useEffect(() => {
         async function fetchData() {
             try {
@@ -30,6 +35,7 @@ function useRecetas() {
 const ConsumirTab = () => {
     const recipes = useRecetas();
     const [selectedRecipes, setSelectedRecipes] = useState<Receta[]>([]);
+    const [foodType, setFoodType] = useState(FoodTypes[0]);
 
     const mappedRecipes = recipes?.map(recipe => ({
         key: recipe.id,
@@ -37,7 +43,7 @@ const ConsumirTab = () => {
         value: recipe.id
     }));
 
-    function addRecipe(event, data) {
+    function addRecipe(_, data) {
         const value = recipes?.find(recipe => recipe.id === data.value);
 
         if (!value) {
@@ -54,58 +60,68 @@ const ConsumirTab = () => {
         setSelectedRecipes(filteredValues);
     }
 
-    function onSubmit() {
-
+    function selectFoodType(_, data) {
+        setFoodType(data.value);
     }
 
-    function renderSelector() {
-        return <ColumnFood>
-            <Form onSubmit={onSubmit}>
-                <Form.Field required>
-                    <label>Recetas</label>
-                    <Dropdown
-                        button
-                        className='icon'
-                        floating
-                        labeled
-                        icon='food'
-                        options={mappedRecipes}
-                        search
-                        text='Selecciona tu Receta'
-                        onChange={addRecipe}
-                    />
-                    <Message header='Selecciona las recetas que deseas consumir'/>
-                </Form.Field>
-                {
-                    selectedRecipes.length > 0 && (
-                        <List divided selection>
-                            {
-                                selectedRecipes.map((recipe) => (
-                                    <List.Item>
-                                        <Label horizontal>{recipe.nombre}</Label>
-                                        <Button
-                                            circular
-                                            color='red'
-                                            icon='trash'
-                                            floated="right"
-                                            onClick={() => removeRecipe(recipe.id)}
-                                        />
-                                    </List.Item>
-                                ))
-                            }
-                        </List>
-                    )
-                }
-                <br/>
-                <Button type='submit' positive floated="right">CONSUMIR</Button>
-            </Form>
-        </ColumnFood>
+    function onSubmit() {
+        console.log({ foodType, selectedRecipes });
     }
 
     return (
         <Tab.Pane loading={recipes === null}>
             {recipes && recipes.length > 0 ? (
-                renderSelector()
+                <ColumnFood>
+                    <Form onSubmit={onSubmit}>
+                        <Form.Field required>
+                            <label>Tipo de comida</label>
+                            <Dropdown
+                                value={foodType.key}
+                                button
+                                className='icon'
+                                labeled
+                                options={FoodTypes}
+                                onChange={selectFoodType}
+                            />
+                        </Form.Field>
+                        <Form.Field required>
+                            <label>Recetas</label>
+                            <Dropdown
+                                button
+                                className='icon'
+                                labeled
+                                icon='food'
+                                options={mappedRecipes}
+                                search
+                                text='Selecciona tu Receta'
+                                onChange={addRecipe}
+                            />
+                            <Message header='Selecciona las recetas que deseas consumir'/>
+                        </Form.Field>
+                        {
+                            selectedRecipes.length > 0 && (
+                                <List divided selection>
+                                    {
+                                        selectedRecipes.map((recipe) => (
+                                            <List.Item>
+                                                <Label horizontal>{recipe.nombre}</Label>
+                                                <Button
+                                                    circular
+                                                    color='red'
+                                                    icon='trash'
+                                                    floated="right"
+                                                    onClick={() => removeRecipe(recipe.id)}
+                                                />
+                                            </List.Item>
+                                        ))
+                                    }
+                                </List>
+                            )
+                        }
+                        <br/>
+                        <Button type='submit' positive floated="right">CONSUMIR</Button>
+                    </Form>
+                </ColumnFood>
             ) : (
                 <div>No hay recetas para mostrar</div>
             )}
